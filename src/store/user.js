8110307ca27b81060c717ede00363f194e73b4b5
kotet912 <kotet912@yandex.ru>
2024-09-27 01:36:36 +0300

    Основной механизм регистрации

--- src/store/user.js
+++ src/store/user.js
@@ -7,26 +7,45 @@ export default ({
       uid: null,
     },
   },
-  getters: {
-  },
+
   mutations: {
+    SET_USER (state, payload) {
+      state.user.isAuthenticated = true
+      state.user.uid = payload
+    },
+    SET_PROCESSING (state, payload) {
+      state.processing = payload
+    },
+    SET_ERROR (state, payload) {
+      state.error = payload
+    },
+    CLEAR_ERROR (state) {
+      state.error = null
+    },
   },
   actions: {
     /* eslint-disable */
     SIGNUP ({ commit }, { email, password }) {
       const auth = getAuth();
+      commit('SET_PROCESSING', true);
       createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-          // Signed up 
           const user = userCredential.user;
-          // ...
+          commit('SET_USER', userCredential.uid);
+          commit('SET_PROCESSING', false);
+
         })
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
-          // ..
+          commit('SET_PROCESSING', false);
+          commit('SET_ERROR', error.message);
+
         });
     },
+  },
+  getters: {
+    isUserAuthenticated: (state) => state.user.isAuthenticated,
   },
   modules: {
   }
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,22 +1,27 @@
 <template>
   <v-container fluid fill-height class="fill-height ">
     <v-layout align-center justify-center class="overflow-visible  ">
-      <v-col xs12 sm8 md4>
+      <v-col xs12 sm8 md6>
         <v-card class="elevation-12 ">
           <v-toolbar dark color="primary ">
             <v-toolbar-title>Регистрация</v-toolbar-title>
           </v-toolbar>
           <v-card-text>
+
+            <v-alert v-if="error" dense border="left" type="warning" :value="error">
+              {{ error }}
+            </v-alert>
             <v-form>
-              <v-text-field prepend-icon="person" name="login" label="E-mail" type="text" required
+              <v-text-field prepend-icon="mdi mdi-account" name="login" label="E-mail" type="text" required
                 v-model="email"></v-text-field>
-              <v-text-field id="password" prepend-icon="lock" name="Пароль" label="Password" type="password" required
-                v-model="password"></v-text-field>
+              <v-text-field id="password" prepend-icon="mdi mdi-lock" name="Пароль" label="Password" type="password"
+                required v-model="password"></v-text-field>
             </v-form>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary rounded-lg" border="primary md" @click.prevent="signup">Зарегистрироваться</v-btn>
+            <v-btn color="primary rounded-lg" border="primary md" @click.prevent="signup"
+              :disabled="processing">Зарегистрироваться</v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -34,8 +39,23 @@ export default {
       password: null
     };
   },
-
-  mounted () {
+  computed: {
+    error () {
+      return this.$store.getters.getError;
+    },
+    processing () {
+      return this.$store.getters.getProcessing;
+    },
+    isUserAuthenticated () {
+      return this.$store.getters.isUserAuthenticated;
+    }
+  },
+  watch: {
+    isUserAuthenticated (authenticated) {
+      if (authenticated) {
+        this.$router.push('/home');
+      }
+    }
   },
 
   methods: {
