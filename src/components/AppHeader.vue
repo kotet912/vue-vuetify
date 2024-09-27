97dcabe87fe4b88574936a466a9cdd6b7b9a4d7b
kotet912 <kotet912@yandex.ru>
2024-09-28 02:22:23 +0300

    модалка для выхода

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,10 +17,30 @@
         <v-btn v-for="(item, i) in menuItems" :key="`item${i}`" :to="item.to" :prepend-icon="item.icon">
           {{ item.text }}
         </v-btn>
-        <v-btn flat @click.prevent="signout" v-if="isUserAuthenticated">
+        <v-btn flat @click.prevent="dialog = !dialog" v-if="isUserAuthenticated">
           <v-icon>mdi mdi-logout</v-icon>
           Выйти
         </v-btn>
+
+        <v-row justify="center">
+          <v-dialog v-model="dialog" persistent max-width="290">
+            <v-card>
+              <v-card-title class="text-h5">
+                Выйти?
+              </v-card-title>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary darken-1" text @click.prevent="signout(); dialog = false">
+                  Да
+                </v-btn>
+                <v-btn color="red darken-1" text @click="dialog = false">
+                  Нет
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-row>
+
       </v-toolbar-items>
     </v-toolbar>
   </div>
@@ -30,7 +50,13 @@
 <script>
 export default {
   name: 'VueVuetifyAppHeader',
+  data () {
+    return {
+      drawer: false,
+      dialog: false,
 
+    };
+  },
   computed: {
     isUserAuthenticated () {
       return this.$store.getters.isUserAuthenticated;
@@ -52,11 +78,7 @@ export default {
         ];
     },
   },
-  data () {
-    return {
-      drawer: false,
-    };
-  },
+
 
   mounted () {
 
